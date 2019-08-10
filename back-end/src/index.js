f563726b1016dbd7c2fdee09b14e35e00d0b3e0f
JongGyun Park <jonggyuni@gmail.com>
2019-08-11 01:55:46 +0900

    setting lint-staged and husky

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,11 +1,11 @@
-function add(a, b) {
-  const c = 1;
-  c = 2;
-
-  if (b) {
-    b = 5;
-  }
-  return a + b;
-}
+// function add(a, b) {
+//   const c = 1;
+//   c = 2;
+//   d = 4;
+//   if (b) {
+//     b = 5;
+//   }
+//   return a + b;
+// }
 
 console.log('hi!!!!!!!!!');
